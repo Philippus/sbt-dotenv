4761b47ea9a332d1d42976b45996f6d31e1c9a34
slasch <slavaschmidt@gmx.de>
2019-10-05 10:49:46 +0200

    Reverted changes in comment formatting

--- src/main/scala/au/com/onegeek/sbtdotenv/SbtDotenv.scala
+++ src/main/scala/au/com/onegeek/sbtdotenv/SbtDotenv.scala
@@ -1,25 +1,25 @@
 /**
-* The MIT License (MIT)
-*
-* Copyright (c) 2014 Matt Fellows (OneGeek)
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy of
-* this software and associated documentation files (the "Software"), to deal in
-* the Software without restriction, including without limitation the rights to
-* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-* the Software, and to permit persons to whom the Software is furnished to do so,
-* subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Matt Fellows (OneGeek)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package au.com.onegeek.sbtdotenv
 
 import sbt.Keys._
@@ -29,11 +29,11 @@ import scala.collection.JavaConverters._
 import scala.io.Source
 
 /**
-* sbt-dotenv - a dotenv (https://github.com/bkeepers/dotenv) implementation for Scala sbt.
-*
-* Reads a file
-*
-*/
+ * sbt-dotenv - a dotenv (https://github.com/bkeepers/dotenv) implementation for Scala sbt.
+ *
+ * Reads a file
+ *
+ */
 object SbtDotenv extends AutoPlugin {
 
   object autoImport {
@@ -65,11 +65,11 @@ object SbtDotenv extends AutoPlugin {
   }
 
   /**
-  * Configures the sbt environment from a dotfile (.env) if one exists.
-  *
-  * @param state
-  * @return
-  */
+   * Configures the sbt environment from a dotfile (.env) if one exists.
+   *
+   * @param state
+   * @return
+   */
   def configureEnvironment(state: State, fileName: String): State =
     loadAndExpand(state, fileName).fold(logNoFile(state, fileName))(applyEnvironment(state))
 
@@ -97,11 +97,11 @@ object SbtDotenv extends AutoPlugin {
   }
 
   /**
-  * Parse provided file in .env format and return an immutable environment Map[String, String]
-  *
-  * @param file .env file to read
-  * @return
-  */
+   * Parse provided file in .env format and return an immutable environment Map[String, String]
+   *
+   * @param file .env file to read
+   * @return
+   */
   def parseFile(file: File): Option[Map[String, String]] = {
     if (!file.exists) {
       None
